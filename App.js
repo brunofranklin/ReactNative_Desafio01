956f9d28ecb94582bc52a68f6c30399d06b23052
Bruno Franklin <you@example.com>
2022-06-27 17:45:48 -0300

    ExercioDesafio01

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Dimensoes = () => {
+
+   return (
+      
+      <View style={{ justifyContent:'flex-start', alignItems: 'center'}}>
+         <View style={{width:390, height:25, backgroundColor:'#50e3c2'}}/>
+      
+         <View style={{ justifyContent:'center', alignItems: 'center', padding:20}}>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/> 
+      </View>
+
+      <View style={{ justifyContent:'center', alignItems: 'center', padding:20}}>
+         <View style={{width:150, height:30, backgroundColor:'#f5a623'}}/> 
+      </View>
+
+      <View style={{ flexDirection:'row', alignItems: 'center'}}>
+         <View style={{width:195, height:78, backgroundColor:'#9013fe'}}/>
+         <View style={{width:195, height:78, backgroundColor:'#4a90e2'}}/> 
+      </View>
+
+      <View style={{ justifyContent:'center', alignItems: 'center'}}>
+      <View style={{width:390, height:13, backgroundColor:'#50e3c2'}}/>
+      </View>
+
+      <View style={{justifyContent:'space-around', flexDirection:'row', alignItems: 'center', padding:30}}>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/>
+      </View>
+
+      <View style={{justifyContent:'space-around', flexDirection:'row', alignItems: 'center', padding:30}}>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/>
+         <View style={{width:100, height:100, backgroundColor:'#f5a623'}}/>
+      </View>
+
+      <View style={{ justifyContent:'flex-end', alignItems: 'center'}}>
+         <View style={{width:390, height:60, backgroundColor:'#4a90e2'}}/>
+      </View> 
+        
+     </View>
+      
+   );
+};
+
+export default Dimensoes;
